ddcf60bd9e2b0aac61bb91da2dc0db665cfccf9f
nhuithanhcong <pquan2212@gmail.com>
2026-08-05 22:11:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function upDate(previewPic) {
+
+    console.log("Sự kiện hover/focus hoạt động");
+    console.log("ALT:", previewPic.alt);
+    console.log("SRC:", previewPic.src);
+
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+
+    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function unDo() {
+
+    document.getElementById("image").style.backgroundImage = "url('')";
+
+
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
